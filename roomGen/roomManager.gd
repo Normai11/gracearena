50343c4582d54441f070ab4f82f43c2c3414c7fb
Normai11 <nerdminecraft56@gmail.com>
2025-12-22 00:21:36 -0600

    late night awakening (WIP)

--- roomGen/roomManager.gd
+++ roomGen/roomManager.gd
@@ -60,30 +60,21 @@ func generate_rooms() -> void:
 	injectNode.add_child.call_deferred(room)
 
 func add_room_shape(child, offset, flip) -> void:
-	var areaRid = PhysicsServer2D.area_create()
-	var shapeRid = PhysicsServer2D.rectangle_shape_create()
-	var spaceState = get_world_2d().direct_space_state
-	PhysicsServer2D.shape_set_data(shapeRid, Vector2(child.roomSize.x, child.roomSize.y))
-	PhysicsServer2D.area_set_collision_layer(areaRid, 10)
-	PhysicsServer2D.area_set_collision_mask(areaRid, 10)
-	PhysicsServer2D.area_set_monitorable(areaRid, true)
-	PhysicsServer2D.area_set_space(areaRid, spaceState)
+	var rectRid : Rect2
 	var shapeCenter = child.roomSize / 2
 	var shapePos : Vector2
 	
+	shapePos.y = child.roomOffset.y + offset.y
 	if flip:
 		shapePos.x = -(child.roomOffset.x + -offset.x)
 	else:
 		shapePos.x = (child.roomOffset.x + offset.x)
-	shapePos.y = child.roomOffset.y + offset.y
-	PhysicsServer2D.area_add_shape(areaRid, shapeRid, Transform2D(0, shapePos - shapeCenter))
-	var debugRect : Rect2 = Rect2(shapePos.x, shapePos.y, child.roomSize.x, child.roomSize.y)
 	
-	debugRect.position = shapePos - shapeCenter
+	rectRid.size = child.roomSize
+	rectRid.position = shapePos
 	
-	var shapeData = debugRect
 	#roomDrawQueue.append([shapeData, Color.PURPLE])
-	roomRIDarray.append(areaRid)
+	roomRIDarray.append(rectRid)
 
 func _draw() -> void:
 	for rect in roomDrawQueue:
@@ -111,97 +102,178 @@ func setup_room(child, flip : bool) -> void:
 			roomOffset.y = (child.roomContObj.position.y + child.position.y)
 
 func check_bounds(checking, original) -> int:
-	var newRoom : int
 	var roomInst
 	var tempDir = roomDir
 	var shapeRid = PhysicsServer2D.rectangle_shape_create()
-	var spaceState = get_world_2d().direct_space_state
-	var centerPoint = checking.roomSize / 2
+	var checkingRect : Rect2
 	var checkPos : Vector2 = Vector2.ZERO
-	var queryParams = PhysicsShapeQueryParameters2D.new()
+	var centerPos : Vector2 = Vector2.ZERO
+	var roomPass : Array[int] = []
+	var conflicts : int = 0
 	
-	PhysicsServer2D.shape_set_data(shapeRid, Vector2(checking.roomSize.x, checking.roomSize.y))
-	queryParams.shape_rid = shapeRid
-	queryParams.collision_mask = 10
-	queryParams.collide_with_areas = true
-	queryParams.collide_with_bodies = false
-	queryParams.margin = 0
-	
-	print(roomOffset)
+	checkingRect.size = checking.roomSize
 	checkPos.y = checking.roomOffset.y + roomOffset.y
 	if !tempDir:
 		checkPos.x = -(checking.roomOffset.x + -roomOffset.x)
 	else:
 		checkPos.x = (checking.roomOffset.x + roomOffset.x)
-	queryParams.transform.origin = checkPos - centerPoint
-	#roomDrawQueue.append([Rect2(checkPos - centerPoint, checking.roomSize), Color.RED])
-	for att in failAttempts:
-		print_rich("[rainbow][wave]" + str(att))
-		var path
-		var oldDir = tempDir
-		var results = spaceState.intersect_shape(queryParams)
-		if results: #INDIAN ERROR
-			for cycle in DataStore.roomPaths.size():
-				queue_redraw()
-				print(cycle)
+	centerPos = checkingRect.size / 2
+	checkingRect.position = checkPos - centerPos
+	
+	roomDrawQueue.append([checkingRect, Color.FIREBRICK])
+	for shape in roomRIDarray.size():
+		var results = checkingRect.intersects(roomRIDarray[shape])
+		if results:
+			var path
+			for room in DataStore.roomPaths.size():
 				checkPos = Vector2.ZERO
-				path = load(DataStore.roomPaths[cycle])
-				newRoom = cycle
+				path = load(DataStore.roomPaths[room])
 				roomInst = path.instantiate()
-				if roomInst.startFlipping:
-					tempDir = !tempDir
-				else:
-					tempDir = oldDir
-				PhysicsServer2D.shape_set_data(shapeRid, Vector2(roomInst.roomSize.x, roomInst.roomSize.y))
-				centerPoint = roomInst.roomSize / 2
+				
+				checkingRect.size = roomInst.roomSize
 				checkPos.y = roomInst.roomOffset.y + roomOffset.y
 				if !tempDir:
 					checkPos.x = -(roomInst.roomOffset.x + -roomOffset.x)
 				else:
 					checkPos.x = (roomInst.roomOffset.x + roomOffset.x)
-				queryParams.shape_rid = shapeRid
-				queryParams.transform.origin = checkPos - centerPoint
-				roomDrawQueue.append([Rect2(checkPos - centerPoint, roomInst.roomSize), Color.INDIAN_RED])
-				results = spaceState.intersect_shape(queryParams, 64)
-				print(results)
+				centerPos = checkingRect.size / 2
+				checkingRect.position = checkPos - centerPos
+				results = checkingRect.intersects(roomRIDarray[shape])
 				if !results:
-					newRoom = cycle
-					## THIS IS THE LAST THORN IN MY PATH TO FREEDOM AND ENJOYMENT, PLEASE FIX THIS
-					#var oldPos = Vector2.ZERO
-					var tempOffset = Vector2.ZERO
+					#roomDrawQueue.append([checkingRect, Color.RED])
+					#final test
+					var tempOffset : Vector2 = Vector2.ZERO
+					var oldDir : bool
 					if tempDir:
 						tempOffset = roomInst.roomContObj.position + checkPos
-						print_rich("[shake]" + str(tempOffset))
 					else:
 						tempOffset.x = -(roomInst.roomContObj.position.x - checkPos.x)
 						tempOffset.y = roomInst.roomContObj.position.y + checkPos.y
-						print_rich("[shake]" + str(tempOffset))
 					if roomInst.startFlipping:
 						oldDir = !tempDir
 					else:
 						oldDir = tempDir
+					
 					for idx in DataStore.roomPaths.size():
-						queue_redraw()
 						checkPos = Vector2.ZERO
 						path = load(DataStore.roomPaths[idx])
 						roomInst = path.instantiate()
-						if roomInst.startFlipping:
-							tempDir = !tempDir
-						else:
-							tempDir = oldDir
-						PhysicsServer2D.shape_set_data(shapeRid, Vector2(roomInst.roomSize.x, roomInst.roomSize.y))
-						centerPoint = roomInst.roomSize / 2
+						
+						checkingRect.size = roomInst.roomSize
 						checkPos.y = roomInst.roomOffset.y + tempOffset.y
 						if !tempDir:
 							checkPos.x = -(roomInst.roomOffset.x + -tempOffset.x)
 						else:
 							checkPos.x = (roomInst.roomOffset.x + tempOffset.x)
-						queryParams.shape_rid = shapeRid
-						queryParams.transform.origin = checkPos - centerPoint
-						results = spaceState.intersect_shape(queryParams)
-						roomDrawQueue.append([Rect2(checkPos - centerPoint, roomInst.roomSize), Color.FIREBRICK])
+						centerPos = checkingRect.size / 2
+						checkingRect.position = checkPos - centerPos
+						
+						
+						results = checkingRect.intersects(roomRIDarray[shape])
 						if !results:
-							return cycle
-		else:
-			return original
+							print("super good!")
+							roomPass.append(room)
+							#roomDrawQueue.append([checkingRect, Color.INDIAN_RED])
+	if roomPass.size() > 0:
+		var rng = RandomNumberGenerator.new()
+		var roll = rng.randi_range(0, roomPass.size() - 1)
+		var chosen = roomPass[roll]
+		return chosen
+	
 	return original
+
+#func check_bounds(checking, original) -> int:
+	#var newRoom : int
+	#var roomInst
+	#var tempDir = roomDir
+	#var shapeRid = PhysicsServer2D.rectangle_shape_create()
+	#var spaceState = get_world_2d().direct_space_state
+	#var centerPoint = checking.roomSize / 2
+	#var checkPos : Vector2 = Vector2.ZERO
+	#var queryParams = PhysicsShapeQueryParameters2D.new()
+	#
+	#PhysicsServer2D.shape_set_data(shapeRid, Vector2(checking.roomSize.x, checking.roomSize.y))
+	#queryParams.shape_rid = shapeRid
+	#queryParams.collision_mask = 10
+	#queryParams.collide_with_areas = true
+	#queryParams.collide_with_bodies = false
+	#queryParams.margin = 0
+	#
+	#print(roomOffset)
+	#checkPos.y = checking.roomOffset.y + roomOffset.y
+	#if !tempDir:
+		#checkPos.x = -(checking.roomOffset.x + -roomOffset.x)
+	#else:
+		#checkPos.x = (checking.roomOffset.x + roomOffset.x)
+	#queryParams.transform.origin = checkPos - centerPoint
+	##roomDrawQueue.append([Rect2(checkPos - centerPoint, checking.roomSize), Color.RED])
+	#for att in failAttempts:
+		#print_rich("[rainbow][wave]" + str(att))
+		#var path
+		#var oldDir = tempDir
+		#var results = spaceState.intersect_shape(queryParams)
+		#if results: #INDIAN ERROR
+			#for cycle in DataStore.roomPaths.size():
+				#queue_redraw()
+				#print(cycle)
+				#checkPos = Vector2.ZERO
+				#path = load(DataStore.roomPaths[cycle])
+				#newRoom = cycle
+				#roomInst = path.instantiate()
+				#if roomInst.startFlipping:
+					#tempDir = !tempDir
+				#else:
+					#tempDir = oldDir
+				#PhysicsServer2D.shape_set_data(shapeRid, Vector2(roomInst.roomSize.x, roomInst.roomSize.y))
+				#centerPoint = roomInst.roomSize / 2
+				#checkPos.y = roomInst.roomOffset.y + roomOffset.y
+				#if !tempDir:
+					#checkPos.x = -(roomInst.roomOffset.x + -roomOffset.x)
+				#else:
+					#checkPos.x = (roomInst.roomOffset.x + roomOffset.x)
+				#queryParams.shape_rid = shapeRid
+				#queryParams.transform.origin = checkPos - centerPoint
+				#roomDrawQueue.append([Rect2(checkPos - centerPoint, roomInst.roomSize), Color.INDIAN_RED])
+				#results = spaceState.intersect_shape(queryParams, 64)
+				#print(results)
+				#if !results:
+					#newRoom = cycle
+					### THIS IS THE LAST THORN IN MY PATH TO FREEDOM AND ENJOYMENT, PLEASE FIX THIS
+					##var oldPos = Vector2.ZERO
+					#var tempOffset = Vector2.ZERO
+					#if tempDir:
+						#tempOffset = roomInst.roomContObj.position + checkPos
+						#print_rich("[shake]" + str(tempOffset))
+					#else:
+						#tempOffset.x = -(roomInst.roomContObj.position.x - checkPos.x)
+						#tempOffset.y = roomInst.roomContObj.position.y + checkPos.y
+						#print_rich("[shake]" + str(tempOffset))
+					#if roomInst.startFlipping:
+						#oldDir = !tempDir
+					#else:
+						#oldDir = tempDir
+					#for idx in DataStore.roomPaths.size():
+						#queue_redraw()
+						#checkPos = Vector2.ZERO
+						#path = load(DataStore.roomPaths[idx])
+						#roomInst = path.instantiate()
+						#if roomInst.startFlipping:
+							#tempDir = !tempDir
+						#else:
+							#tempDir = oldDir
+						#PhysicsServer2D.shape_set_data(shapeRid, Vector2(roomInst.roomSize.x, roomInst.roomSize.y))
+						#centerPoint = roomInst.roomSize / 2
+						#checkPos.y = roomInst.roomOffset.y + tempOffset.y
+						#if !tempDir:
+							#checkPos.x = -(roomInst.roomOffset.x + -tempOffset.x)
+						#else:
+							#checkPos.x = (roomInst.roomOffset.x + tempOffset.x)
+						#queryParams.shape_rid = shapeRid
+						#queryParams.transform.origin = checkPos - centerPoint
+						#results = spaceState.intersect_shape(queryParams)
+						#roomDrawQueue.append([Rect2(checkPos - centerPoint, roomInst.roomSize), Color.FIREBRICK])
+						#if !results:
+							#return cycle
+		#else:
+			#return original
+	#return original
